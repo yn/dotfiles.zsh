c1c74d782c76ceeb07ed83ce1def56b464e2dd18
Yuri Niyazov <yuri.niyazov@gmail.com>
2016-12-15 01:38:16 -0800

    Remove currently not used git functions, remove double_underscore before other used functions

--- yn.zsh
+++ yn.zsh
@@ -1,14 +1,5 @@
 # vim: ts=2 sw=2 et
-__yn::git::exists() {
-  command -v git >/dev/null 2>&1 
-}
-
-__yn::git::abort_missing() {
-  echo >&2 "Git is not installed."
-  exit 1
-}
-
-__yn::ohmyzsh::setup() {
+yn::ohmyzsh::setup() {
   export ZSH="${ZDOTDIR:-$HOME}/.oh-my-zsh"
   if ! test -d "$ZSH/.git"; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -16,13 +7,13 @@ __yn::ohmyzsh::setup() {
   source "${ZDOTDIR:-$HOME}/ohmyzsh.zshrc"
 }
 
-__yn::zpl::setup() {
+yn::zpl::setup() {
   export ZPLG_HOME="${ZDOTDIR:-$HOME}/.zplugin"
   if ! test -d "$ZPLG_HOME/bin/.git"; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/psprint/zplugin/master/doc/install.sh)"
   fi;
 }
 
-__yn::rvm::setup() {
+yn::rvm::setup() {
   [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 }
